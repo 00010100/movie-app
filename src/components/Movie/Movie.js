a35425f0725c1452b6afd8e35ed2f7a253b1624b
00010100 <0001101004@protonmail.com>
2018-11-10 20:15:02 +0200

    Add tooltip

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 import './Movie.scss';
 
@@ -6,8 +7,9 @@ const Movie = ({ movie }) => {
   const { poster_path, title } = movie;
 
   return (
-    <div className="Movie">
+    <div className="Movie" data-tip={title} data-for="movie">
       <img className="Movie__img" src={poster_path} alt={title} />
+      <ReactTooltip id="movie" />
     </div>
   );
 };
